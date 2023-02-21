eea33d8211d615666351b6541230d11609a47f09
rian-fernandes-souza@hotmail.com <rian-fernandes-souza@hotmail.com>
2023-02-21 01:10:51 +0000

    finalizado modelo dim_produtos

--- models/staging/erp_northwind/stg_erp__produtos.sql
+++ models/staging/erp_northwind/stg_erp__produtos.sql
@@ -4,13 +4,12 @@ with
             cast(product_id as int) as id_produto
             , cast(supplier_id as int) as id_fornecedor 				
             , cast(category_id as int) as id_categoria	
-
             , cast(product_name	as string) as nome_produto								
             , cast(quantity_per_unit as string)	as quantidade_por_unidade			
             , cast(unit_price as numeric) as preco_por_unidade 					
             , cast(units_in_stock as int) as unidades_no_estoque				
             , cast(units_on_order as int) as unidades_por_ordem				
-            , cast(reorder_level as int) as level_de_pedido				
+            , cast(reorder_level as int) as nivel_reabastecimento				
             , case
                 when discontinued = 1 then true
                 else false 
